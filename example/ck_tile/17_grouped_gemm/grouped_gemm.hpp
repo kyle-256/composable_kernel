6d905b474d0a07aeb9a067891429463780b92fbc
kyle-256 <xianzhao@amd.com>
2025-08-27 02:42:37 +0000

    set NumDTensor=1

--- example/ck_tile/17_grouped_gemm/grouped_gemm.hpp
+++ example/ck_tile/17_grouped_gemm/grouped_gemm.hpp
@@ -154,7 +154,7 @@ struct PipelineTypeTraits<CK_TILE_PIPELINE_COMPUTE_V4>
     using UniversalGemmPipeline = ck_tile::BaseGemmPipelineAgBgCrCompV4<PipelineProblem>;
 };
 
-using grouped_gemm_kargs = ck_tile::GroupedGemmHostArgs<2>;
+using grouped_gemm_kargs = ck_tile::GroupedGemmHostArgs<1>;
 
 auto create_args(int argc, char* argv[])
 {
@@ -182,7 +182,7 @@ auto create_args(int argc, char* argv[])
 
 inline std::size_t get_workspace_size(const std::vector<grouped_gemm_kargs>& gemm_descs)
 {
-    return gemm_descs.size() * sizeof(ck_tile::GemmTransKernelArg<2>);
+    return gemm_descs.size() * sizeof(ck_tile::GemmTransKernelArg<1>);
 }
 
 template <typename ADataType,
